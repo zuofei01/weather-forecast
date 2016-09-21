8db7d467e57cf74ddcd9692e55ccbcdaad346e2a
zuofei <673450811@qq.com>
2016-09-21 16:53:41 +0800

    init project

---
+++ app/src/main/java/com/example/zuofei/weatherforecast/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.zuofei.weatherforecast;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zuofei on 2016/9/21.
+ */
+public class MainActivity extends Activity {
+    @Override    protected void onCreate(Bundle savedInstanceState) {        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);} }
